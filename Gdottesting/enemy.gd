5fca6922f260c370b4aa50eda402e35eb79db103
jamesrayment <137951943+jamesrayment@users.noreply.github.com>
2024-08-01 12:10:52 +1200

    Completed enemy vision cone
    Vision cone is working. Detects player when in view, and doesn't when player passes behind obstacles.

--- Gdottesting/enemy.gd
+++ Gdottesting/enemy.gd
@@ -7,8 +7,6 @@ const JUMP_VELOCITY = 4.5
 #enemy health
 var health = 100
 
-@onready var sightcast = get_node("/root/Node3D/enemy/sightcast")
-
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
@@ -20,15 +18,29 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 		
-	if sightcast.is_colliding():
-			var target = sightcast.get_collider()
-			print(target)
-			if target.is_in_group("player"):
-				print("Ha!")
-		
 	if health <= 0:
 		queue_free()
 
 	move_and_slide()
 
 	
+
+
+func _on_vision_timer_timeout():
+	var overlaps = $VisionCone.get_overlapping_bodies()
+	if overlaps.size() > 0:
+		for overlap in overlaps:
+			if overlap.is_in_group("player"):
+				var playerposition = overlap.global_transform.origin
+				$VisionRaycast.look_at(playerposition, Vector3.UP)
+				$VisionRaycast.force_raycast_update()
+				
+				if $VisionRaycast.is_colliding():
+					var collider = $VisionRaycast.get_collider()
+					
+					if collider.is_in_group("player"):
+						$VisionRaycast.debug_shape_custom_color = Color(174, 0, 0)
+						print("Target spotted")
+					else:
+						$VisionRaycast.debug_shape_custom_color = Color(0, 255, 0)
+						print("Target lost")
