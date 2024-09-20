4dd7d5a8c733185e5b0989e54b9f6c2897997db7
jamesrayment <137951943+jamesrayment@users.noreply.github.com>
2024-09-20 14:58:48 +1200

    Setup basic upgrade system
    Set up scene for player upgrades - money system working and player able to purchase an upgrade when enough money is earnt.

--- Gdottesting/upgradescreen.gd
+++ Gdottesting/upgradescreen.gd
@@ -5,14 +5,18 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	get_node("progbar").set_value(global.upgradelevel * 10)
+	global.money = 60
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	get_node("Label").text = str("money=", global.money)
+	get_node("progbar").set_value(global.upgradelevel * 10)
 
 
 func _on_button_pressed():
-	if global.money == 50:
+	if global.money >= 50:
 		global.upgradelevel += 1
+		global.money -= 50
+	else:
+		pass
