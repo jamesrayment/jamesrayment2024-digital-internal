adfffe7e25d34f18d10c1e831589b1c2eea2afc7
jamesrayment <137951943+jamesrayment@users.noreply.github.com>
2024-10-24 12:20:48 +1300

    Instructions created
    Tells my player how to play my game

---
+++ Gdottesting/instructionscreen.gd
@@ -0,0 +1,25 @@
+extends Node2D
+
+
+var instructions_open = false
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$Label.hide()
+	$Label2.hide()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _input(event):
+	if event.is_action_pressed("Controls") and instructions_open == false:
+		$Label.show()
+		$Label2.show()
+		instructions_open = true
+	elif event.is_action_pressed("Controls") and instructions_open == true:
+		$Label.hide()
+		$Label2.hide()
+		instructions_open = false
